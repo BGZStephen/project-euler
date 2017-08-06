23bc61c484b4ecd38b9b5b1e3457c7da8165b1d9
Stephen Wright <sjw948@gmail.com>
2017-08-06 19:22:11 +0100

    split to provide pretty and clean (performance) options for outputs

--- 1-20/1-multiples-of-3-and-5.js
+++ 1-20/1-multiples-of-3-and-5.js
@@ -1,10 +1,15 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
-
 const chalk = require('chalk');
 const log = console.log;
 
-function x() {
+if (process.argv[2] == 'pretty') {
+  pretty();
+} else {
+  clean();
+}
+
+function pretty() {
   console.time('Completion time')
   let sumValue = 0
   for(let i = 0; i < 1000; i++) {
@@ -20,4 +25,14 @@ function x() {
   console.timeEnd('Completion time')
 }
 
-x()
+function clean() {
+  console.time('Completion time')
+  let sumValue = 0
+  for(let i = 0; i < 1000; i++) {
+    if(i % 3 == 0 || i % 5 == 0) {
+      sumValue += i;
+    }
+  }
+  log(sumValue)
+  console.timeEnd('Completion time')
+}
