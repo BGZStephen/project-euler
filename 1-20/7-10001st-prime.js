699208b151ba98dafd9d674d13c66dbdef98ea37
Stephen Wright <sjw948@gmail.com>
2017-08-08 08:01:24 +0100

    finished 7: 10001st prime

---
+++ 1-20/7-10001st-prime.js
@@ -0,0 +1,36 @@
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+// What is the 10 001st prime number?
+
+const log = console.log;
+
+async function clean() {
+  console.time('Completion time')
+  let knownPrimes = 0;
+  let thenThousandAndFirstPrime;
+  for(let i = 2; knownPrimes < 10001; i++) {
+    const numberChecked = await isPrime(i)
+    if (numberChecked) {
+      knownPrimes += 1
+      thenThousandAndFirstPrime = i;
+    }
+  }
+  log(thenThousandAndFirstPrime)
+  console.timeEnd('Completion time');
+}
+
+
+function isPrime(i) {
+  let j = 1;
+  let divisors = 0
+  for(j; j <= i; j++) {
+    if (i % j == 0) {
+      divisors ++
+    }
+    if ( divisors > 2 ){
+      return false;
+    }
+  }
+  return i;
+}
+
+clean();
