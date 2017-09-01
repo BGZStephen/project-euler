1bd49de45996b124faf803f8df349b489cfd9c65
Stephen Wright <sjw948@gmail.com>
2017-09-01 19:30:37 +0100

    Revert "built home view and added responsive stylings / lorum text"
    This reverts commit 3892f3bc7cdf0050d54624678f0581b4aee74c99.

--- 1-20/10-summation-of-primes.js
+++ 1-20/10-summation-of-primes.js
@@ -3,64 +3,46 @@
 
 const log = console.log;
 
-let primesArray = 0;
+const primesArray = []
 
-function clean() {
-  for(let i = 90; i <= 100; i++) {
-    testPrimality(i)
-  }
-}
-
-function testPrimality(number) {
-  const sqrt = Math.round(Math.sqrt(number))
-  for(let i = 2; i < sqrt; i++) {
-    if(sqrt % i !== 0) {
-      return
+async function clean() {
+  console.time('Completion time')
+  let primeSum = 0
+  for(let i = 2; i < 2000000; i++) {
+    if(primesArray.length < 10) {
+      if(testPrime(i)) {
+        primeSum += i;
+        primesArray.push(i)
+      }
+    } else if(primesArray.length >= 10) {
+      if(testPrimeAgainstArray(i)) {
+        primesArray.push(i)
+        primeSum += i;
+      }
     }
-    primesArray += sqrt
   }
+  log(primeSum)
+  console.timeEnd('Completion time');
 }
 
-console.log(primesArray)
+function testPrime(i) {
+  let j = 2;
+  let divisors = 0
+  for(j; j < i; j++) {
+    if (i % j == 0) {
+      return false
+    }
+  }
+  return i;
+}
 
-// function clean() {
-//   console.time('Completion time')
-//   let primeSum = 0
-//   for(let i = 2; i < 2000000; i++) {
-//     if(primesArray.length < 10) {
-//       if(testPrime(i)) {
-//         primeSum += i;
-//         primesArray.push(i)
-//       }
-//     } else if(primesArray.length >= 10) {
-//       if(testPrimeAgainstArray(i)) {
-//         primesArray.push(i)
-//         primeSum += i;
-//       }
-//     }
-//   }
-//   log(primeSum)
-//   console.timeEnd('Completion time');
-// }
-//
-// function testPrime(i) {
-//   let j = 2;
-//   let divisors = 0
-//   for(j; j < i; j++) {
-//     if (i % j == 0) {
-//       return false
-//     }
-//   }
-//   return i;
-// }
-//
-// function testPrimeAgainstArray(i) {
-//   for(let j = 1; j <= primesArray.length; j++) {
-//     if (i % primesArray[j] == 0) {
-//       return false
-//     }
-//   }
-//   return i;
-// }
+function testPrimeAgainstArray(i) {
+  for(let j = 1; j <= primesArray.length; j++) {
+    if (i % primesArray[j] == 0) {
+      return false
+    }
+  }
+  return i;
+}
 
 clean();
