3892f3bc7cdf0050d54624678f0581b4aee74c99
Stephen Wright <sjw948@gmail.com>
2017-09-01 19:30:37 +0100

    built home view and added responsive stylings / lorum text

--- 1-20/10-summation-of-primes.js
+++ 1-20/10-summation-of-primes.js
@@ -3,46 +3,64 @@
 
 const log = console.log;
 
-const primesArray = []
+let primesArray = 0;
 
-async function clean() {
-  console.time('Completion time')
-  let primeSum = 0
-  for(let i = 2; i < 2000000; i++) {
-    if(primesArray.length < 10) {
-      if(testPrime(i)) {
-        primeSum += i;
-        primesArray.push(i)
-      }
-    } else if(primesArray.length >= 10) {
-      if(testPrimeAgainstArray(i)) {
-        primesArray.push(i)
-        primeSum += i;
-      }
-    }
+function clean() {
+  for(let i = 90; i <= 100; i++) {
+    testPrimality(i)
   }
-  log(primeSum)
-  console.timeEnd('Completion time');
 }
 
-function testPrime(i) {
-  let j = 2;
-  let divisors = 0
-  for(j; j < i; j++) {
-    if (i % j == 0) {
-      return false
+function testPrimality(number) {
+  const sqrt = Math.round(Math.sqrt(number))
+  for(let i = 2; i < sqrt; i++) {
+    if(sqrt % i !== 0) {
+      return
     }
+    primesArray += sqrt
   }
-  return i;
 }
 
-function testPrimeAgainstArray(i) {
-  for(let j = 1; j <= primesArray.length; j++) {
-    if (i % primesArray[j] == 0) {
-      return false
-    }
-  }
-  return i;
-}
+console.log(primesArray)
+
+// function clean() {
+//   console.time('Completion time')
+//   let primeSum = 0
+//   for(let i = 2; i < 2000000; i++) {
+//     if(primesArray.length < 10) {
+//       if(testPrime(i)) {
+//         primeSum += i;
+//         primesArray.push(i)
+//       }
+//     } else if(primesArray.length >= 10) {
+//       if(testPrimeAgainstArray(i)) {
+//         primesArray.push(i)
+//         primeSum += i;
+//       }
+//     }
+//   }
+//   log(primeSum)
+//   console.timeEnd('Completion time');
+// }
+//
+// function testPrime(i) {
+//   let j = 2;
+//   let divisors = 0
+//   for(j; j < i; j++) {
+//     if (i % j == 0) {
+//       return false
+//     }
+//   }
+//   return i;
+// }
+//
+// function testPrimeAgainstArray(i) {
+//   for(let j = 1; j <= primesArray.length; j++) {
+//     if (i % primesArray[j] == 0) {
+//       return false
+//     }
+//   }
+//   return i;
+// }
 
 clean();
